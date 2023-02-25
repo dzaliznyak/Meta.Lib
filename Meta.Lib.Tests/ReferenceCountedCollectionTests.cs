b801ea2cd030bf7f1eacd8069513a420fc0cf565
dzaliznyak <dzaliznyak@gmail.com>
2023-02-25 19:57:24 +0200

    Added ReferenceCountedCollection

---
+++ Meta.Lib.Tests/ReferenceCountedCollectionTests.cs
@@ -0,0 +1,82 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Meta.Lib.Tests
+{
+    [TestClass]
+    public class ReferenceCountedCollectionTests
+    {
+        [TestMethod]
+        public void Add_IncreasesReferenceCount()
+        {
+            // Arrange
+            var collection = new ReferenceCountedCollection<string>();
+
+            // Act
+            collection.Add("foo");
+
+            // Assert
+            Assert.AreEqual(1, collection.GetReferenceCount("foo"));
+        }
+
+        [TestMethod]
+        public void Add_MultipleTimes_IncrementsReferenceCount()
+        {
+            // Arrange
+            var collection = new ReferenceCountedCollection<string>();
+
+            // Act
+            collection.Add("foo");
+            collection.Add("foo");
+
+            // Assert
+            Assert.AreEqual(2, collection.GetReferenceCount("foo"));
+        }
+
+        [TestMethod]
+        public void Remove_DecreasesReferenceCount()
+        {
+            // Arrange
+            var collection = new ReferenceCountedCollection<string>();
+            collection.Add("foo");
+            collection.Add("foo");
+
+            // Act
+            collection.Remove("foo");
+
+            // Assert
+            Assert.AreEqual(1, collection.GetReferenceCount("foo"));
+        }
+
+        [TestMethod]
+        public void Remove_RemovesItemWhenReferenceCountIsZero()
+        {
+            // Arrange
+            var collection = new ReferenceCountedCollection<string>();
+            collection.Add("foo");
+
+            // Act
+            collection.Remove("foo");
+
+            // Assert
+            Assert.IsFalse(collection.GetItems().Contains("foo"));
+        }
+
+        [TestMethod]
+        public void GetItems_ReturnsAllItemsWithNonZeroReferenceCounts()
+        {
+            // Arrange
+            var collection = new ReferenceCountedCollection<string>();
+            collection.Add("foo");
+            collection.Add("bar");
+            collection.Remove("foo");
+
+            // Act
+            var items = collection.GetItems();
+
+            // Assert
+            Assert.AreEqual(1, items.Count());
+            Assert.IsTrue(items.Contains("bar"));
+        }
+    }
+}
---
+++ Meta.Lib/Utils/ReferenceCountedCollection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public class ReferenceCountedCollection<T>
+{
+    readonly Dictionary<T, int> _referenceCounts = new Dictionary<T, int>();
+
+    public void Add(T item)
+    {
+        if (!_referenceCounts.ContainsKey(item))
+        {
+            _referenceCounts[item] = 1;
+        }
+        else
+        {
+            _referenceCounts[item]++;
+        }
+    }
+
+    public void Remove(T item)
+    {
+        if (_referenceCounts.ContainsKey(item))
+        {
+            _referenceCounts[item]--;
+            if (_referenceCounts[item] == 0)
+            {
+                _referenceCounts.Remove(item);
+            }
+        }
+    }
+
+    public IEnumerable<T> GetItems()
+    {
+        return _referenceCounts.Keys;
+    }
+
+    public int GetReferenceCount(T item)
+    {
+        return _referenceCounts.TryGetValue(item, out int count) ? count : 0;
+    }
+}
