97b1d33af2a79ceca29dc58c01e1d7c6d8a89d3a
dzaliznyak <dzaliznyak@gmail.com>
2024-01-17 12:49:30 +0200

    TestClient update

---
+++ TestClient/Client.cs
@@ -0,0 +1,67 @@
+﻿using Meta.Lib.Messages;
+using Meta.Lib.Modules.PubSub;
+using Meta.Lib.Modules.PubSubPipe;
+using System;
+using System.Threading.Tasks;
+
+namespace TestClient;
+
+internal class Client : IAsyncDisposable
+{
+    MetaPubSub _hub;
+    PubSubPipeClient _pipe;
+    static int pingId;
+
+    public Client()
+    {
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _pipe.UnsubscribeOnServer<PingResponse>();
+        _hub.Unsubscribe<PingResponse>(OnPingResponse);
+        _pipe.DisconnectFromServer();
+        _pipe.Dispose();
+    }
+
+    public async Task Init()
+    {
+        try
+        {
+            _hub = new MetaPubSub();
+
+            // connecting the remote server
+            _pipe = new PubSubPipeClient("Meta", _hub);
+            await _pipe.ConnectToServer();
+            await _pipe.SubscribeOnServer<PingResponse>();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static Task OnPingResponse(PingResponse response)
+    {
+        Console.WriteLine($"Client >>>: ping response {response.Id} {DateTime.Now:HH:mm:ss.fff}");
+        return Task.CompletedTask;
+    }
+
+    public async Task Ping()
+    {
+        try
+        {
+            await _pipe.PublishOnServer(new PingCommand() { Id = pingId++.ToString() });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"PublishOnServer error: {ex.Message}");
+        }
+    }
+
+    internal async Task Disconnect()
+    {
+        await _pipe.UnsubscribeOnServer<PingResponse>();
+        _pipe.DisconnectFromServer();
+    }
+}
--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -1,30 +1,50 @@
-﻿using Meta.Lib.Messages;
-using Meta.Lib.Modules.PubSub;
-using Meta.Lib.Modules.PubSubPipe;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
-namespace TestClient
-{
-    class Program
-    {
-        static MetaPubSub hub;
-        static PubSubPipeClient pipe;
-        static int pingId;
+namespace TestClient;
 
-        static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
+    {
+        string line;
+        while ((line = Console.ReadLine()) != "exit")
         {
-            RunClient();
-            string line;
-            while ((line = Console.ReadLine()) != "exit")
+            if (line == "ping" || line == "")
             {
-                if (line == "ping" || line == "")
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        await using var client = new Client();
+                        await client.Init();
+                        await client.Ping();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"PublishOnServer error: {ex.Message}");
+                    }
+                });
+            }
+            else if (line == "test")
+            {
+                for (int i = 0; i < 5; i++)
                 {
                     Task.Run(async () =>
                     {
                         try
                         {
-                            await pipe.PublishOnServer(new PingCommand() { Id = pingId++.ToString() });
+                            await using var client = new Client();
+                            await client.Init();
+
+                            for (int i = 0; i < 100; i++)
+                            {
+                                await client.Ping();
+                            }
+
+                            // first Dispose will unsubscribe from server
+                            // so we need delay to finish all tasks
+                            await Task.Delay(1000);
                         }
                         catch (Exception ex)
                         {
@@ -33,33 +53,6 @@ namespace TestClient
                     });
                 }
             }
-        }
-
-        static async void RunClient()
-        {
-            try
-            {
-                // local hub creation
-                hub = new MetaPubSub();
-
-                // connecting the remote server
-                pipe = new PubSubPipeClient("Meta", hub);
-                await pipe.ConnectToServer();
-                await pipe.SubscribeOnServer<PingResponse>();
-
-                // subscribing
-                hub.Subscribe<PingResponse>(OnPingResponse);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        static Task OnPingResponse(PingResponse response)
-        {
-            Console.WriteLine($"Client >>>: ping response {response.Id} {DateTime.Now:HH:mm:ss.fff}");
-            return Task.CompletedTask;
         }
     }
 }
